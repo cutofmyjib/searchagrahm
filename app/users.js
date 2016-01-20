49bc467d46fb841224e0e7f479c5dc4153857110
cutofmyjib <thedianalozano@gmail.com>
2016-01-20 00:17:08 -0800

    users component

---
+++ app/users.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import Header from './header.js';
+
+export default class Users extends Component {
+  render() {
+    return (
+      <h3>users results here</h3>
+    );
+  }
+}
