aed6412eb0af52cbcf4b29a34ce12e25867b57da
cutofmyjib <thedianalozano@gmail.com>
2016-01-21 15:41:39 -0800

    add link component

--- app/user.js
+++ app/user.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import {Link} from 'react-router'
 
 export default class User extends Component {
   render() {
     return (
-      <div className="card">
-        <div className="image">
+      <div className="red card">
+        <Link to="user1" className="image">
           <img src={this.props.profile_picture} />
-        </div>
+        </Link>
         <div className="content">
-          <a className="header">{this.props.username}</a>
+          <div className="header">{this.props.username}</div>
           <div className="meta">{this.props.full_name} {this.props.lastName}</div>
         </div>
       </div>
