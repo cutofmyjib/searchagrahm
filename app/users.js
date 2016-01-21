c82285993f609a72e5584c733325d3ffa88d715e
cutofmyjib <thedianalozano@gmail.com>
2016-01-21 00:46:10 -0800

    use reqwest to get jsonp

--- app/users.js
+++ app/users.js
@@ -1,28 +1,35 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import {connect} from 'react-refetch'
+import reqwest from 'reqwest';
 import User from './user.js';
 
 export default class Users extends Component {
-  render() {
-    var usersData = [
-      {
-        imgSrc: "http://thefuturists.ca/wp-content/uploads/2010/10/DieAntwoordPoster1.jpg",
-        userName: "Die_Antwoord",
-        firstName: "Dee",
-        lastName: "Antwoord"
-      },
-      {
-        imgSrc: "https://s-media-cache-ak0.pinimg.com/736x/68/99/97/689997e5bcce1ac339738232f87a8855.jpg",
-        userName: "Grimez_Antwoord",
-        firstName: "Actually",
-        lastName: "Grimes"
-      }
-      ];
+  constructor(props) {
+    super(props);
+    this.state = { type: 'start' };
+  }
 
-    var userCards = usersData.map(function(data){
+  componentDidMount() {
+    this.setState({ type: 'loading' });
+
+    reqwest({
+      url: 'https://api.instagram.com/v1/users/search?q=dana&access_token=337924724.c2da448.945bb99df5c44b4e9ef5b69d6e0859ee',
+      type: 'jsonp',
+      method: 'get'
+    })
+    .then(response => this.setState({ type: 'success', response }))
+    .fail(err => this.setState( { type: 'error' }));
+  }
+
+  render() {
+    var userCards = []
+    if (this.state.type === 'success') {
+      userCards = this.state.response.data.map(function(data){
         return <User {...data} />
-    });
+      });
+    }
+
+
     return (
       <div className="results-body">
         <div className="ui link cards">
