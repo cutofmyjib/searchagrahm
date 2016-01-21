8817b2d847d07d5bb1682523d08c2bf9d57cc377
cutofmyjib <thedianalozano@gmail.com>
2016-01-20 19:38:45 -0800

    comment out test data

--- app/users.js
+++ app/users.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
-import Header from './header.js';
+import {connect} from 'react-refetch'
+import User from './user.js';
 
-export default class Users extends Component {
+class Users extends Component {
   render() {
+    // var usersData = [
+    //   {
+    //     imgSrc: "http://thefuturists.ca/wp-content/uploads/2010/10/DieAntwoordPoster1.jpg",
+    //     userName: "Die_Antwoord",
+    //     firstName: "Dee",
+    //     lastName: "Antwoord"
+    //   },
+    //   {
+    //     imgSrc: "https://s-media-cache-ak0.pinimg.com/736x/68/99/97/689997e5bcce1ac339738232f87a8855.jpg",
+    //     userName: "Grimez_Antwoord",
+    //     firstName: "Actually",
+    //     lastName: "Grimes"
+    //   }
+    //   ];
+
+    var userCards = usersData.map(function(data){
+        return <User {...data} />
+    });
     return (
-      <h3>users results here</h3>
+      <div className="results-body">
+        <div className="ui link cards">
+          {userCards}
+        </div>
+      </div>
     );
   }
 }
+
+export default connect(props => ({
+  usersFetch: `/users/${props.userId}`
+}))(Profile)
