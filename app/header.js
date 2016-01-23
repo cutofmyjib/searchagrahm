3aab93496c6f7db36c7dee3141072aebb2fb5536
cutofmyjib <thedianalozano@gmail.com>
2016-01-22 20:50:51 -0800

    make header name a link + styling

--- app/header.js
+++ app/header.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import { Link } from 'react-router'
 
 export default class Header extends Component {
   render() {
     return (
       <header className="site-header">
-        <h3 className="site-header-heading">Searchagrahm</h3>
+        <h2 className="site-header-heading"><Link to="/" className="site-header-heading-link">Searchagrahm</Link></h2>
         <div className="ui search site-header-center">
           <div className="ui icon input">
             <input className="prompt" type="text" placeholder="socialprintstudio..." />
