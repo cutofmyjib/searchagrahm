58c5d2ccb3e88b4e7ca70b340dbff3640f53d95b
cutofmyjib <thedianalozano@gmail.com>
2016-01-22 23:27:10 -0800

    different render elements

--- app/users.js
+++ app/users.js
@@ -9,7 +9,7 @@ export default class Users extends Component {
     this.state = { type: 'start' };
   }
 
-  componentDidMount() {
+  getContent() {
     this.setState({ type: 'loading' });
 
     reqwest({
@@ -21,21 +21,60 @@ export default class Users extends Component {
     .fail(err => this.setState( { type: 'error' }));
   }
 
+  componentDidMount() {
+    this.getContent();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.location.query.username !== prevProps.location.query.username) {
+      this.getContent();
+    }
+  }
+
   render() {
-    var userCards = []
+    var userCards = [];
     if (this.state.type === 'success') {
+      if (this.state.response.data.length < 1) {
+        return (
+          <div className="ui info message">
+            <div className="header">
+              Sorry, couldn&apos;t find a username &apos;{this.props.location.query.username}&apos;.
+            </div>
+          </div>
+        );
+      }
       userCards = this.state.response.data.map(function(data){
         return <User {...data} />
       });
+      return (
+        <div className="results-body">
+          <h3 className="ui header">Search results for '{this.props.location.query.username}'</h3>
+          <div className="ui link cards">
+            {userCards}
+          </div>
+        </div>
+      );
+    } else if (this.state.type === 'error') {
+      return (
+        <div className="ui error message">
+          <div className="header">
+            Oops something happened!
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="ui icon message">
+          <i className="notched circle loading icon"></i>
+          <div className="content">
+            <div className="header">
+              Just one second
+            </div>
+            <p>Fetching content for you.</p>
+          </div>
+        </div>
+      );
     }
 
-
-    return (
-      <div className="results-body">
-        <div className="ui link cards">
-          {userCards}
-        </div>
-      </div>
-    );
   }
 }
