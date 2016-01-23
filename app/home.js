371a7d85e60fc84146975967bb1fa1f65199aa47
cutofmyjib <thedianalozano@gmail.com>
2016-01-23 15:20:20 -0800

    import searchbox component

--- app/home.js
+++ app/home.js
@@ -1,27 +1,15 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 import { hashHistory, Router, Route, Link } from 'react-router'
+import Searchbox from './searchbox.js';
 
 export default class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  redirectSearch(event) {
-    var queryString = event.target.value;
-    queryString = queryString.replace(/\s/g, '');
-    if (event.keyCode === 13 && queryString) {
-      hashHistory.push('/users?username=' + queryString)
-    }
-  }
-
   render() {
     return (
       <div className="home">
         <div className="ui search">
           <div className="ui icon input">
-            <input className="prompt" type="text" onKeyDown={this.redirectSearch.bind(this)} placeholder="socialprintstudio..." />
+            <Searchbox />
             <i className="search icon"></i>
           </div>
         </div>
