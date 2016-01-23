14b0a0f3047aad14308c4ac736eb63468d248cab
cutofmyjib <thedianalozano@gmail.com>
2016-01-22 20:20:17 -0800

    add properties to input, use hashhistory to redirect

--- app/home.js
+++ app/home.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
 import {render} from 'react-dom';
+import { hashHistory, Router, Route, Link } from 'react-router'
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { media: '' };
+  }
+
+  redirectSearch(event) {
+    if (event.keyCode === 13) {
+      hashHistory.push('/users?username=' + this.state.username)
+    }
+  }
+
+  captureString(event) {
+    this.setState({ username: event.target.value });
+  }
+
   render() {
     return (
       <div className="home">
         <div className="ui search">
           <div className="ui icon input">
-            <input className="prompt" type="text" placeholder="socialprintstudio..." />
+            <input className="prompt" type="text" onKeyDown={this.redirectSearch.bind(this)} onChange={this.captureString.bind(this)} placeholder="socialprintstudio..." />
             <i className="search icon"></i>
           </div>
         </div>
